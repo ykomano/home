3c256cd386d339092387cb4aa749b19b371b2eb5
ykomano <yoshikazu.komano@kii.com>
2013-12-20 11:33:37 +0900

    add setting for bold font

--- app/rxvt-unicode.app/Contents/MacOS/core.sh
+++ app/rxvt-unicode.app/Contents/MacOS/core.sh
@@ -3,4 +3,4 @@
 export LC_CTYPE=UTF-8
 cd
 
-$HOME/bin/urxvt -geometry 100x30 -rv -fn '-*-terminus ja-medium-r-*-*-16-*-*-*-*-*-iso10646-1' +sb -tn xterm-256color &
+$HOME/bin/urxvt -geometry 100x30 -rv -fn '-*-terminus ja-medium-r-*-*-16-*-*-*-*-*-iso10646-1' -fb '-*-terminus ja-medium-r-*-*-16-*-*-*-*-*-iso10646-1' +sb -tn xterm-256color &
