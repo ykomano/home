ed848dbdb1fbb765d503144bc9dde12f883c2d6c
ykomano <yoshikazu.komano@kii.com>
2014-08-01 18:49:04 +0900

    add uim default setting

---
+++ dotfile/.uim.d/customs/custom-prime-advanced.scm
@@ -0,0 +1,8 @@
+(define prime-nr-candidate-max 10)
+(define prime-always-show-window? #t)
+(define prime-custom-display-usage? #f)
+(define prime-custom-display-comment? #f)
+(define prime-custom-display-form? #f)
+(define prime-custom-number-selection? #f)
+(define prime-custom-app-mode-vi? #f)
+(define prime-pseudo-mode-cursor? #f)
